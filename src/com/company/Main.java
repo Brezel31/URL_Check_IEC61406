b5985da417b3ddbf99ed055a3b81ef5a81d193be
GEIGERP1 <GeigerP1@H5CG71825K3>
2022-06-20 15:52:27 +0200

    small fix

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -30,7 +30,7 @@ public class Main {
     }
     private static void checkCase(String yourDomain){
         if (!yourDomain.toLowerCase().equals(yourDomain)){
-            System.out.println("Großbuchstaben sind im Path nicht erlaubt"); //"Capital letters are not allowed in the path"
+            System.out.println("Capital letters are not allowed in the path");
         }
     }
 
@@ -44,7 +44,7 @@ public class Main {
             // if domainAvailability = "UNAVAILABLE" everything is ok, it seems to be a valid domain
             System.out.println(s.useDelimiter("\\A").next());
         } catch (MalformedURLException error) {
-            System.out.println("Fehler bei der URL-Erstellung: " + yourDomain); //"Error during URL creation: "
+            System.out.println("Error during URL creation:  " + yourDomain);
         } catch (Exception exception) {
             System.out.println("Exception for: " + yourDomain); //"URL not valid: "
         }
@@ -69,15 +69,15 @@ public class Main {
     }
     private static void checkString(String url){
         if (url.contains("xn--")){
-            System.out.println("Kann Punycode enthalten"); //"May contain punycode"
+            System.out.println("May contain punycode");
         }if (url.length() >255){
-            System.out.println("URL hat mehr als die erlaubten 255 Zeichen"); //"URL has more than the allowed 255 characters"
+            System.out.println("URL has more than the allowed 255 characters");
         }
         Pattern p = Pattern.compile("[^A-Za-z0-9#,$&'()*+\\-./~\\[\\]=?:;!_@]");
         Matcher m = p.matcher(url);
         while (m.find()){
             // findings are not allowed characters, if nothing is found, everything is fine
-           System.out.println("Ungültiges Zeichen an Stelle: " + m.start() + " - " + m.end()); //"Invalid character in place: "
+           System.out.println("Invalid character in place: " + m.start() + " - " + m.end());
         }
     }
 }
